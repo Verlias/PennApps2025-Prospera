d5386ba90e5cf22dff1e14a03258139088e643a2
Aman Sharma <mail.aman@protonmail.com>
2024-09-21 22:39:06 -0400

    more ui stuff

--- client/src/components/SpendChart.jsx
+++ client/src/components/SpendChart.jsx
@@ -3,18 +3,8 @@ import {useEffect, useState} from "react";
 import {getGroupedTransactions} from "../utils/tools.js";
 import {CAPITALONE_KEY} from "../../keys.js";
 
-export const SpendChart = () => {
+export const SpendChart = ({yVals}) => {
     const xValues = Array.from({ length: 31}).map((_, i) => i + 1);
-    const [yVals, setYVals] = useState([])
-
-    useEffect(() => {
-        const customerId = "66eede179683f20dd5189f63";
-        const baseUrl = "http://api.nessieisreal.com";
-        const url = `${baseUrl}/accounts/${customerId}/purchases?key=${CAPITALONE_KEY}`;
-
-        fetch(url).then(res => res.json())
-            .then(resp => setYVals(getGroupedTransactions(resp)))
-    }, [])
 
     return (
         <Plot
@@ -38,7 +28,7 @@ export const SpendChart = () => {
                 }
             ]}
             layout={{
-                width: "1000",
+                width: "1100",
                 height: "500",
                 paper_bgcolor: 'rgba(0,0,0,0)',
                 plot_bgcolor: "rgba(0,0,0,0)",
